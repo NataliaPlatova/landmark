8fe233d3907fc61fe4d765af34f9d030c1eda08d
Natalia Platova <nataliaplatova47@gmail.com>
2022-03-01 18:05:10 +0700

    add actions

--- src/data/commentsActions.js
+++ src/data/commentsActions.js
@@ -3,6 +3,7 @@ import { FETCH_COMMENTS } from "./types";
 export function fetchComments() {
     return async dispatch => {
         const res = await fetch(`https://jsonplaceholder.typicode.com/users?_limit=20`);
+        //const res = await fetch(`https://landmark-e09dc-default-rtdb.asia-southeast1.firebasedatabase.app/comments.json`)
         const comments = await res.json();
         dispatch({
             type: FETCH_COMMENTS,
